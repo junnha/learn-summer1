5adb105d440abc372de33d27e310cc66940f8ba6
junnha <junha320@gmail.com>
2025-06-22 22:21:56 +0900

    ps: Solved

---
+++ ps/30030.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int x,y;
+    std::cin >> x;
+    if(x == 1) {
+        std::cout << "SciComLove";
+    } else if (x==2) {
+        std::cout << "SciComLove\nSciComLove";
+    }
+
+}
